2f9f3846d4e6003696eeedca6838cbc5a0f4a515
biel4s <kamilbielawski13@gmail.com>
2023-10-18 20:32:35 +0200

    Changed input type to password

--- pages/login.vue
+++ pages/login.vue
@@ -4,7 +4,7 @@
       <label for="login"></label>
       <input type="text" class="form-group" placeholder="Login" id="login" required>
       <label for="password"></label>
-      <input type="text" class="form-group" placeholder="Password" id="password" required>
+      <input type="password" class="form-group" placeholder="Password" id="password" required>
 
       <button type="submit" class="btn form-group">Log in</button>
     </form>
