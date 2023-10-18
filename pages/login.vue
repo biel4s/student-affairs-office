2813de848a8d04675ffd8060072ba468568c3b78
biel4s <kamilbielawski13@gmail.com>
2023-10-18 20:28:58 +0200

    Added login form

---
+++ pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <form>
+      <label for="login"></label>
+      <input type="text" class="form-group" placeholder="Login" id="login" required>
+      <label for="password"></label>
+      <input type="text" class="form-group" placeholder="Password" id="password" required>
+
+      <button type="submit" class="btn form-group">Log in</button>
+    </form>
+  </div>
+</template>
