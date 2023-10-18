790f8e81b274b9c0b72758eb71a232d3573c5f29
biel4s <kamilbielawski13@gmail.com>
2023-10-19 01:04:36 +0200

    Added not working auth method

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="container">
-    <form>
+    <div class="custom-shape-divider-top-1697668358">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+      </svg>
+    </div>
+    <img src="~/assets/images/logo.png" class="logo" alt="logo">
+    <form @submit.prevent="auth">
       <label for="login"></label>
-      <input type="text" class="form-group" placeholder="Login" id="login" required>
+      <input v-model="login" type="text" class="form-group" id="login" placeholder="Login" required>
       <label for="password"></label>
-      <input type="password" class="form-group" placeholder="Password" id="password" required>
-
+      <input v-model="password" type="password" class="form-group" id="password" placeholder="Password" required>
       <button type="submit" class="btn form-group">Log in</button>
     </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      login: '',
+      password: '',
+    };
+  },
+  methods: {
+    auth() {
+      console.log(this.login, this.password)
+    }
+  }
+}
+</script>
