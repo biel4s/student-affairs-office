d76038bf2eb9f65c7b73c3b32f12ad43db684ad6
biel4s <kamilbielawski13@gmail.com>
2023-10-19 17:03:45 +0200

    Added media queries

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="custom-shape-divider-top-1697668358">
+    <div class="custom-shape-divider-top-1697724837">
       <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
       </svg>
